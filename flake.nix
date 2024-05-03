4f01800b106f6ef84891509ad8ffacd60a331385
diniamo <diniamo53@gmail.com>
2024-05-03 13:56:55 +0200

    fix: outputs should be a function

--- flake.nix
+++ flake.nix
@@ -1,10 +1,12 @@
 {
   description = "My collection of flake templates";
 
-  outputs.templates = {
-    rust = {
-      path = ./rust;
-      description = "A simple Rust template, without Naersk";
+  outputs = _: {
+    templates = {
+      rust = {
+        path = ./rust;
+        description = "A simple Rust template, without Naersk";
+      };
     };
   };
 }
